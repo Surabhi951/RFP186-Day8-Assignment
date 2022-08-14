ce05ae1dea5bea9b4347ed03302092436c41f73a
Surabhi951 <surabhibhagat95@gmail.com>
2022-08-14 13:36:53 +0530

    Added: display message

--- src/com/bridgelabz/lineComparison/LineComparison.java
+++ src/com/bridgelabz/lineComparison/LineComparison.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.lineComparison;
 
 public class LineComparison {
+
     public static void main(String[] args) {
         System.out.println("Welcome to line comparison computation program");
     }
